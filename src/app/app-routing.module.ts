732e86e26f6d95c2498dfe4adbd65e5ba4dfb82c
Gustavo Duarte <gustavoduarte@MacBook-Pro-de-Gustavo.local>
2024-08-15 12:11:47 -0300

    Creating component weather-home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WeatherHomeComponent } from './modules/weather/page/weather-home/weather-home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', //Quando o caminho for vazio (ao inicializar a aplicação)
+    redirectTo: 'weather', //Será redirecionado para a rota weather
+    pathMatch: 'full'
+  },
+  {
+    path: 'weather', //Rota weather
+    component: WeatherHomeComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {HttpClientModule} from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WeatherHomeComponent } from './modules/weather/page/weather-home/weather-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    WeatherHomeComponent
   ],
   imports: [
     BrowserModule,
@@ -17,6 +19,7 @@ import { AppComponent } from './app.component';
     BrowserAnimationsModule,
     HttpClientModule,
     FormsModule,
+    FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
